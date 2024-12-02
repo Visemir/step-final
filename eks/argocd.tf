42055df7631b0f2fc181c43b9ec9dc55b9c0371b
Visemir <kalinichenko.o.v@gmail.com>
2024-12-02 13:33:32 +0200

    add coments

--- eks/argocd.tf
+++ eks/argocd.tf
@@ -1,55 +1,54 @@
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  chart      = "argo-cd"
-  repository = "https://argoproj.github.io/argo-helm"
+  name       = "argocd"  # Ім'я релізу Helm
+  chart      = "argo-cd"  # Назва чарту Helm для ArgoCD
+  repository = "https://argoproj.github.io/argo-helm" # Репозиторій, звідки береться чар
   version    = "6.6.0"
 
-  namespace       = "argocd"
-  create_namespace = true
+  namespace       = "argocd"  # Простір імен, у якому буде розгорнуто ArgoCD
+  create_namespace = true  # Створює простір імен, якщо він не існує
 
   set {
-    # Run server without TLS
-    name  = "configs.params.server\\.insecure"
+    name  = "configs.params.server\\.insecure" # Налаштування для запуску сервера без TLS в нашому випадку трафік  дешифрується на лоадбалансері
     value = true
   }
 
-  set {
+  set { # Налаштування глобального домену для ArgoCD
     name  = "global.domain"
     value = "argocd.olegk.devops3.test-danit.com"
   }
 
-  set {
+  set { # Увімкнення Ingress для сервера ArgoCD
     name  = "server.ingress.enabled"
     value = "true"
   }
 
-  set {
+  set { # Встановлення класу Ingress-контролера
     name  = "server.ingress.ingressClassName"
     value = "nginx"
   }
 
    
 
-  set {
+  set { # Відключення примусового перенаправлення на HTTPS
     name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/force-ssl-redirect"
     value = "false"
   }
 
-  set {
+  set {   # Увімкнення підтримки SSL-passthrough
     name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/ssl-passthrough"
     value = "true"
   }
 
   
-  set {
+  set {  # Протокол бекенда встановлений як HTTP
     name  = "server.ingress.backendProtocol"
     value = "HTTP"  
   }
 
-  set {
-    name  = "configs.secret.argocdServerAdminPassword"
+  set {   # Встановлення пароля адміністратора ArgoCD
+    name  = "configs.secret.argocdServerAdminPassword" 
     value = "$2a$10$WttnEprScw2HSAEuw7GF0OZPi0tZgodgQEH.KYmYYv.0NvMRKYCCa"
   }
 
-  depends_on = [helm_release.nginx_ingress]
+  depends_on = [helm_release.nginx_ingress] # Встановлення залежності від релізу Helm для NGINX Ingress
 }
